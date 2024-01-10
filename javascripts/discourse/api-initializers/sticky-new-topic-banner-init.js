64f117f832e4ae1b17d5c29d3c56546b25fd2885
Don <71207900+VaperinaDEV@users.noreply.github.com>
2024-01-10 20:44:43 +0100

    REVERT: Sticky New Topic Banner PM

--- javascripts/discourse/api-initializers/sticky-new-topic-banner-init.js
+++ javascripts/discourse/api-initializers/sticky-new-topic-banner-init.js
@@ -39,6 +39,20 @@ export default {
           tracker.resetTracking();
         }
       });
+
+      // Sticky New Topic Banner PM
+      api.modifyClass("controller:user-topics-list", {
+        pluginId: "sticky-new-topics-banner",
+        @action
+        showInserted(event) {
+          event?.preventDefault();
+          this.model.loadBefore(this.pmTopicTrackingState.newIncoming);
+          this.pmTopicTrackingState.resetIncomingTracking();
+
+          const userNavigation = document.querySelector(".user-primary-navigation");
+          userNavigation.scrollIntoView();
+        }
+      }); 
     });
   },
 };
