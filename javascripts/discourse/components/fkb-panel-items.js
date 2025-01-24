f6bfed401fd1c1fbaf877f0c5f2449f0b0398cd0
Don <71207900+VaperinaDEV@users.noreply.github.com>
2025-01-24 13:27:39 +0100

    Update and rename fkb-panel-items.js to fkb-panel-items.gjs

--- javascripts/discourse/components/fkb-panel-items.js
+++
@@ -1,6 +0,0 @@
-import Component from "@glimmer/component";
-import { tracked } from "@glimmer/tracking";
-
-export default class FKBPanel extends Component {
-  @tracked fkbPanelItems = JSON.parse(settings.fkb_panel_items);
-}
