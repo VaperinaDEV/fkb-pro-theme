4471ab79270d21780c4286414f2d4c30d731ebe3
Don <71207900+VaperinaDEV@users.noreply.github.com>
2025-01-29 11:12:06 +0100

    Add toggle-bulk-select ValueTransformer

---
+++ javascripts/discourse/api-initializers/toggle-bulk-select.js
@@ -0,0 +1,30 @@
+import { withPluginApi } from "discourse/lib/plugin-api";
+
+export default {
+  name: "toggle-bulk",
+  initialize() {
+    withPluginApi("1.33.0", (api) => {
+
+      api.addValueTransformerName("toggle-bulk-click");
+
+      api.registerValueTransformer("toggle-bulk-click", ({ value, context }) => {
+        const onClick = (sel, callback) => {
+          let target = context.event?.target.closest(sel);
+          if (target) {
+            callback(target);
+          }
+        };
+
+        if (typeof value === "function") {
+          value(context.event);
+        }
+
+        onClick("button.bulk-select", () => {
+          document.body.classList.toggle("bulk-select-enabled");
+        });
+
+        return value;
+      });
+    });
+  }
+};
