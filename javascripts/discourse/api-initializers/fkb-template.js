39ad47f4334cd01aa31baffd938610931f3200c7
Don <71207900+VaperinaDEV@users.noreply.github.com>
2025-01-31 22:45:06 +0100

    Update fkb-template.js

--- javascripts/discourse/api-initializers/fkb-template.js
+++ javascripts/discourse/api-initializers/fkb-template.js
@@ -1,41 +1,47 @@
-import { withPluginApi } from "discourse/lib/plugin-api";
+import { apiInitializer } from "discourse/lib/api";
+import TliTopSection from "../components/topic-list-item/tli-top-section";
+import TliMiddleSection from "../components/topic-list-item/tli-middle-section";
 
-export default {
-  name: "fkbpro",
-  initialize() {
-    withPluginApi("1.8.0", (api) => {
+export default apiInitializer("1.8.0", (api) => {
 
-      api.registerValueTransformer("topic-list-columns", ({ value: columns }) => {
-        if (!settings.disable_topic_list_modification) {
-          columns.delete("posters");
-          columns.delete("replies");
-          columns.delete("views");
-          columns.delete("activity");
-        }
-        return columns;
-      });
+  api.registerValueTransformer("topic-list-item-mobile-layout", () => false);
 
-      api.modifyClass("component:discovery/topics", {
-        pluginId: "new-new",
+  api.registerValueTransformer("topic-list-columns", ({ value: columns }) => {
+    if (!settings.disable_topic_list_modification) {
+      columns.delete("posters");
+      columns.delete("replies");
+      columns.delete("views");
+      columns.delete("activity");
+    }
+    
+    return columns;
+  });
+
+  api.renderInOutlet("topic-list-before-link", TliTopSection);
+  api.renderInOutlet("topic-list-main-link-bottom", TliMiddleSection);
+
+  api.modifyClass(
+    "component:discovery/topics",
+    (Superclass) =>
+      class extends Superclass { 
         get renderNewListHeaderControls() {
           return (
             this.showTopicsAndRepliesToggle &&
             !this.args.bulkSelectEnabled
           );
         }
-      });
+      }
+  );
       
-      api.onPageChange((url, title) => {
-        const fkbHidden = localStorage.getItem("fkb_panel_hidden") === "true";
-        const fkbVisible = localStorage.getItem("fkb_panel_hidden") === "false";
-        const isHidden = document.body.classList.contains("fkb-panel-hidden");
+  api.onPageChange((url, title) => {
+    const fkbHidden = localStorage.getItem("fkb_panel_hidden") === "true";
+    const fkbVisible = localStorage.getItem("fkb_panel_hidden") === "false";
+    const isHidden = document.body.classList.contains("fkb-panel-hidden");
         
-        if (fkbHidden && !isHidden) {
-          document.body.classList.add("fkb-panel-hidden");
-        } else if (fkbVisible && isHidden) {
-          document.body.classList.remove("fkb-panel-hidden");
-        }
-      });
-    });
-  },
-};
+    if (fkbHidden && !isHidden) {
+      document.body.classList.add("fkb-panel-hidden");
+    } else if (fkbVisible && isHidden) {
+      document.body.classList.remove("fkb-panel-hidden");
+    }
+  });
+});
