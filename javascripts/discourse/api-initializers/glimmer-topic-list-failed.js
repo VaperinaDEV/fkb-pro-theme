5cf9bcda2c7240f16c057d68debf166718209d29
Don <71207900+VaperinaDEV@users.noreply.github.com>
2025-02-04 12:36:00 +0100

    FIX: removes mobile view restriction to use the correct style on desktop too

--- javascripts/discourse/api-initializers/glimmer-topic-list-failed.js
+++ javascripts/discourse/api-initializers/glimmer-topic-list-failed.js
@@ -3,7 +3,7 @@ import { apiInitializer } from "discourse/lib/api";
 export default apiInitializer("1.8.0", (api) => {  
   const site = api.container.lookup("site:main");
 
-  if (site.mobileView && !site.useGlimmerTopicList) {
+  if (!site.useGlimmerTopicList) {
     document.body.classList.add("glimmer-topic-list-failed");
   }
 });
