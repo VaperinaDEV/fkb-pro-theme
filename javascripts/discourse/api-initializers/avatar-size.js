d879000d2c01832571734bb491efc9d01bafdb29
Don <71207900+VaperinaDEV@users.noreply.github.com>
2025-08-01 19:53:37 +0200

    COMPATIBILITY: Update to work with Glimmer post stream

--- javascripts/discourse/api-initializers/avatar-size.js
+++ javascripts/discourse/api-initializers/avatar-size.js
@@ -1,10 +1,34 @@
-import { apiInitializer } from "discourse/lib/api";
+import { withSilencedDeprecations } from "discourse/lib/deprecated";
+import { withPluginApi } from "discourse/lib/plugin-api";
 
-export default apiInitializer("1.8.0", (api) => {
-  const site = api.container.lookup("site:main");
-  
+function avatarSize(api) {
   // Change avatar size on desktop
-  if (!site.mobileView) {
-    api.changeWidgetSetting("post-avatar", "size", 60);
-  }
-});
+  api.registerValueTransformer(
+    "post-avatar-size",
+    () => 60
+  );
+  
+  // wrap the old widget code silencing the deprecation warnings
+  withSilencedDeprecations("discourse.post-stream-widget-overrides", () =>
+    oldAvatarSize(api)
+  );
+}
+
+// old widget code
+function oldAvatarSize(api) {
+  // Change avatar size on desktop
+  api.changeWidgetSetting("post-avatar", "size", 60);
+}
+
+export default {
+  name: "avatar-size",
+  initialize(container) {
+    const site = api.container.lookup("site:main");
+
+    if (!site.mobileView) {
+      withPluginApi((api) => {
+        avatarSize(api);
+      });
+    }
+  },
+};
