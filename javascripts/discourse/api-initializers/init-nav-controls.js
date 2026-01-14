7757f5e2941a9cbcf8920a0870c72ab3b9f24588
Don <71207900+VaperinaDEV@users.noreply.github.com>
2026-01-14 06:59:03 +0100

    Update init-nav-controls.js

--- javascripts/discourse/api-initializers/init-nav-controls.js
+++ javascripts/discourse/api-initializers/init-nav-controls.js
@@ -17,23 +17,27 @@ export default {
       window.addEventListener('scroll', function() {
         const caps = api.container.lookup("service:capabilities");
 
-        if (!caps.viewport.sm) {
-          return;
+        if (caps.viewport.sm) {
+          scrollTop = window.scrollY;
+          if (
+            lastScrollTop < scrollTop &&
+            scrollTop > scrollMax &&
+            !body.classList.contains(hiddenNavClass)
+          ) { 
+            add_class_on_scroll();
+          } else if (
+            lastScrollTop > scrollTop &&
+            body.classList.contains(hiddenNavClass)
+          ) { 
+            remove_class_on_scroll();
+          }
+          lastScrollTop = scrollTop;
+        } else {
+          // Ensure the class is removed when switching to desktop mode
+          if (body.classList.contains(hiddenNavClass)) {
+            remove_class_on_scroll();
+          }
         }
-        scrollTop = window.scrollY;
-        if (
-          lastScrollTop < scrollTop &&
-          scrollTop > scrollMax &&
-          !body.classList.contains(hiddenNavClass)
-        ) { 
-          add_class_on_scroll();
-        } else if (
-          lastScrollTop > scrollTop &&
-          body.classList.contains(hiddenNavClass)
-        ) { 
-          remove_class_on_scroll();
-        }
-        lastScrollTop = scrollTop;
       });
     });
   },
